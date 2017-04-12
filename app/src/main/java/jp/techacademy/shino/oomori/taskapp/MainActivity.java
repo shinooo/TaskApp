5271b865f666b9f5228f058a81e1705a0f639269
Shino <shino_1109@hotmail.com>
2017-04-13 06:41:55 +0900

    Add function(6）

--- app/src/main/java/jp/techacademy/shino/oomori/taskapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/shino/oomori/taskapp/MainActivity.java
@@ -1,8 +1,11 @@
 package jp.techacademy.shino.oomori.taskapp;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -22,6 +25,8 @@ import io.realm.RealmResults;
 import io.realm.Sort;
 
 public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_TASK = "jp.techacademy.shino.oomori.taskapp.TASK";
+
     private Realm mRealm;
     private RealmChangeListener mRealmListener = new RealmChangeListener() {
         @Override
@@ -46,8 +51,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, InputActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -64,7 +69,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 // 入力・編集する画面に遷移させる
-                Log.d("Debug","List Clicked");
+                Task task = (Task) parent.getAdapter().getItem(position);
+
+                Intent intent = new Intent(MainActivity.this, InputActivity.class);
+                intent.putExtra(EXTRA_TASK, task.getId());
+
+                startActivity(intent);
             }
         });
 
@@ -74,13 +84,35 @@ public class MainActivity extends AppCompatActivity {
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
 
                 // タスクを削除する
+                final Task task = (Task) parent.getAdapter().getItem(position);
+
+                // ダイアログを表示する
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+                builder.setTitle("削除");
+                builder.setMessage(task.getTitle() + "を削除しますか");
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        RealmResults<Task> results = mRealm.where(Task.class).equalTo("id", task.getId()).findAll();
+
+                        mRealm.beginTransaction();
+                        results.deleteAllFromRealm();
+                        mRealm.commitTransaction();
+
+                        reloadListView();
+                    }
+                });
+                builder.setNegativeButton("CANCEL", null);
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
 
                 return true;
             }
         });
 
-        addTaskForTest();
-        
         reloadListView();
     }
 
